99343c19b56a4cb9747f743e41b5bd179b744352
Eric Perez <ericricardoperez@gmail.com>
2014-05-07 10:06:19 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fourwheel::Application.config.secret_key_base = '83043f670924c522354bd326aeb1438eff49037760913448929593157c227b32dfc6fa975e0ff6cbfccbd1c14b98e26c7588e1a1cb6eb129fdaa9726c51db102'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fourwheel::Application.config.session_store :cookie_store, key: '_fourwheel_session'
