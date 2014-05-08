8d0284fef8b90b7e5bf7b2834cfd60efbb8f131c
Eric Perez <ericricardoperez@gmail.com>
2014-05-07 17:38:07 -0700

    fixed bug

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,4 +1,4 @@
 class Follow < ActiveRecord::Base
-  belongs_to :followee, class: 'User'
-  belongs_to :follower, class: 'User'
+  belongs_to :followee, class_name: 'User'
+  belongs_to :follower, class_name: 'User'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :inward_follows, class: 'Follow', foreign_key: 'followee_id'
-  has_many :outward_follows, class: 'Follow', foreign_key: 'follower_id'
+  has_many :inward_follows, class_name: 'Follow', foreign_key: 'followee_id'
+  has_many :outward_follows, class_name: 'Follow', foreign_key: 'follower_id'
 
   has_many :followers, through: :inward_follows, source: :follower
   has_many :followees, through: :outward_follows, source: :followee
